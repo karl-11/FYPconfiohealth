6176db6799913988f303553fbb50132752d1c10a
FengMuzhi <73466837+FengMuzhi@users.noreply.github.com>
2023-01-17 22:02:06 +0800

    booking data console.log

--- frontend/public/booking.js
+++ frontend/public/booking.js
@@ -1,4 +1,19 @@
+
+
+
 function bookingfunction() {
-console.log(response.data.result);
-       
+    // data extraction
+    const date = document.getElementById('bookdate').value;
+    const time = document.getElementById('booktime').value;
+    const location = document.getElementById('booklocation').value;
+
+// data compilation
+const requestBody = {
+    date: date,
+    time: time,
+    location: location,
+};
+
+    console.log(requestBody);
+
     }
