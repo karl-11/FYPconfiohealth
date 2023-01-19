f104e2b0f4c6d895e9a64daaf0158ff13b6234f8
Zay-Han <82993831+Zay-Han@users.noreply.github.com>
2023-01-19 15:08:15 +0800

    created healthprofile.js model file

---
+++ backend/model/healthprofile.js
@@ -0,0 +1,231 @@
+console.log("----------------------------");
+console.log("user.js");
+console.log("----------------------------");
+
+//-----------------------------------------
+// imports
+//-----------------------------------------
+var db = require('../config/dbconfig.js');
+var bcrypt = require('bcrypt');
+var jwt = require('jsonwebtoken');
+var jwtconfig = require('../config/jwtconfig.js')
+
+//-----------------------------------------
+// objects / functions
+//-----------------------------------------
+var hpDB = {
+
+    getAllHPGeneral: function (userid, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                        *
+                    FROM 
+                        HP_General
+                    WHERE
+                        userid = ?;
+                `;
+
+        conn.query(sql, [userid],function (err, result) {
+
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                return callback(null, result);
+            }
+        });
+    },
+
+    getAllHPMedical: function (userid, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                        *
+                    FROM 
+                        HP_Medical
+                    WHERE
+                        userid = ?;
+                `;
+
+        conn.query(sql, [userid],function (err, result) {
+
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                return callback(null, result);
+            }
+        });
+    },
+
+    getAllHPMedication: function (userid, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                        *
+                    FROM 
+                        HP_Medication
+                    WHERE
+                        userid = ?;
+                `;
+
+        conn.query(sql, [userid],function (err, result) {
+
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                return callback(null, result);
+            }
+        });
+    },
+
+    getAllHPSurgical: function (userid, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                        *
+                    FROM 
+                        HP_Surgical
+                    WHERE
+                        userid = ?;
+                `;
+
+        conn.query(sql, [userid],function (err, result) {
+
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                return callback(null, result);
+            }
+        });
+    },
+
+    getAllHPDrug: function (userid, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                        *
+                    FROM 
+                        HP_Drug
+                    WHERE
+                        userid = ?;
+                `;
+
+        conn.query(sql, [userid],function (err, result) {
+
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                return callback(null, result);
+            }
+        });
+    },
+
+    getAllHPVaccination: function (userid, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                        *
+                    FROM 
+                        HP_Vaccination
+                    WHERE
+                        userid = ?;
+                `;
+
+        conn.query(sql, [userid],function (err, result) {
+
+            if (err) {
+                console.log(err);
+                return callback(err, null);
+            } else {
+                return callback(null, result);
+            }
+        });
+    },
+
+    logIn: function (email, password, callback) {
+        var conn = db;
+
+        var sql = ` SELECT 
+                            * 
+                        FROM 
+                            users
+                        WHERE 
+                            email=? 
+                    `;
+
+        conn.query(sql, [email], (error, results) => {
+
+            if (error) {
+                callback(error, null);
+                return;
+            }
+
+            if (results.length === 0) {
+                return callback(null, null);
+
+            }
+            else {
+
+                console.log(results);
+
+                if (bcrypt.compareSync(password, results[0].password) == true) {
+
+                    let data = {
+                        user_id: results[0].id,
+                        role_name: results[0].type,
+                        token: jwt.sign({ id: results[0].id }, jwtconfig, {
+                            expiresIn: 86400 //Expires in 24 hrs
+                        })
+                    }; //End of data variable setup
+
+                    //return res.status(200).json(data);
+                    const user = results[0];
+                    return callback(null, user);
+                } else {
+                    // return res.status(500).json({ message: 'Login has failed.' });
+                    return res.status(500).json({ message: error });
+                }
+
+
+
+            }
+        });
+    },
+
+    signUp: function (email, password, full_name, callback) {
+        var conn = db;
+
+        bcrypt.hash(password, 10, async (err, hash) => {
+            if (err) {
+                console.log('Error on hashing password');
+                return res.status(500).json({ statusMessage: 'Unable to complete registration' });
+            } else {
+                var sql = `
+                INSERT INTO
+                users
+                (email, password, full_name)
+                VALUES 
+                (?, ?, ?);
+                `;
+                conn.query(sql, [email, hash, full_name], function (err, result) {
+                    if (err) {
+                        console.log(err);
+                        return callback(err, null);
+                    } else {
+                        return callback(null, result);
+                    }
+                });
+            }
+        });
+    }
+}
+
+//-----------------------------------------
+// exports
+//-----------------------------------------
+module.exports = userDB;
