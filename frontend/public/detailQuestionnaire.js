013e23bb9d469c9b610b5079405f70bc5c3e7a7b
Karl <k4rlchristian@gmail.com>
2023-01-27 23:59:44 +0800

    fixed button looping

--- frontend/public/detailQuestionnaire.js
+++ frontend/public/detailQuestionnaire.js
@@ -10,7 +10,8 @@ $(document).ready(function () {
     axios.post(`${baseUrl}/getQnsByQnr`, requestBody).then(function (response) {
         for (i = 0; i < response.data.length; i++) {
             const { id, content, max_score } = response.data[i]
-            if (id == 1) {
+            var buttonID = "button" + i;
+            if (i == 0) {
                 var activeStatusClass = "carousel-item active"
             } else {
                 var activeStatusClass = "carousel-item"
@@ -24,7 +25,11 @@ $(document).ready(function () {
                     <!-- question -->
                     <h4 class="m-0 pb-3"><strong>${content}</strong></h4>
                         <!-- 1-10 ranking buttons -->
-                        <div id="buttons">
+                        <div class="btn-toolbar mb-3" role="toolbar" aria-label="Toolbar with button groups">
+                        <div class="btn-group mr-2" role="group" aria-label="First group">
+                        <div id="${buttonID}">
+                        </div>
+                        </div>
                         </div>
                 </div>
             </div>
@@ -33,15 +38,99 @@ $(document).ready(function () {
         }
         for (i = 0; i < response.data.length; i++) {
             const { id, content, max_score } = response.data[i]
-            for (i = 1; i - 1 < max_score; i++) {
-                $('#buttons').append(`
-                    <button onclick="score()" type="button" class="score_${i} btn btn btn-outline-secondary">${i}</button>
+            var buttonID = "button" + i;
+            for (j = 1; (j - 1) < max_score; j++) {
+                $(`#${buttonID}`).append(`
+                    <button onclick="score${j}()" type="button" class="btn btn btn-outline-secondary" id="score_${j}">${j}</button>
                 `)
             }
+            $(document).ready(function () {
+                $('button').click(function () {
+                    $('button').addClass(' disabled');
+                });
+            });
+            $(document).ready(function () {
+                $('.carousel-control-next').click(function () {
+                    $('button').removeClass(' disabled');
+                });
+            });
         }
+
     })
         .catch((error) => {
             console.log(error);
         });
 })
+const totalScore = [];
+function score1() {
+    var button = document.getElementById('score_1');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score2() {
+    var button = document.getElementById('score_2');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score3() {
+    var button = document.getElementById('score_3');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score4() {
+    var button = document.getElementById('score_4');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score5() {
+    var button = document.getElementById('score_5');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score6() {
+    var button = document.getElementById('score_6');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score7() {
+    var button = document.getElementById('score_7');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score8() {
+    var button = document.getElementById('score_8');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score9() {
+    var button = document.getElementById('score_9');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+function score10() {
+    var button = document.getElementById('score_10');
+    var text = button.innerText;
+    console.log(text)
+    totalScore.push(text);
+    console.log(totalScore);
+}
+
 
