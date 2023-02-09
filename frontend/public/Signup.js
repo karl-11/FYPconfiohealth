eeebd293a6b9af66d7a9d74ea6408bcfc0e7e73a
Jia Hua <jiahua.20@ichat.sp.edu.sg>
2023-02-10 00:55:48 +0800

    signup login responsive

--- frontend/public/Signup.js
+++ frontend/public/Signup.js
@@ -11,26 +11,34 @@ form.addEventListener('submit', (event) => {
     const fullname = document.getElementById('name').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('pwd').value;
+    const cfmpassword = document.getElementById('cfmpwd').value;
+
+    if (password != cfmpassword) {
+        alert("Password not same")
+        return
+    }
+    else {
+        // data compilation
+        const requestBody = {
+            full_name: fullname,
+            email: email,
+            password: password
+        };
+
+        console.log("---------------- compiled data -----------");
+        console.log(requestBody);
+
+        axios.post(`${baseUrl}/signup`, requestBody)
+            .then((response) => {
+                const token = response.data.token;
+                console.log("========= signup response ==========")
+                console.log(response.data)
+                window.location.href = "/login";
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
 
 
-    // data compilation
-    const requestBody = {
-        full_name: fullname,
-        email: email,
-        password: password
-    };
-
-    console.log("---------------- compiled data -----------");
-    console.log(requestBody);
-
-    axios.post(`${baseUrl}/signup`, requestBody)
-        .then((response) => {
-            const token = response.data.token;
-            console.log("========= signup response ==========")
-            console.log(response.data)
-            window.location.href = "/login";
-        })
-        .catch((error) => {
-            console.log(error);
-        });
 });
